fbd62b7cfa088d007be8ce9fcb86ac897915a5c8
Tumer124 <egor.bugaev2015@yandex.ru>
2023-04-19 21:38:00 +0700

    create html

--- core/Todo/models.py
+++ core/Todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+	title = models.CharField('Task', max_length = 40)
+	complete = models.BooleanField('Done', default = False)
+	
+	def __str__(self):
+		return self.title
+	
+	
+	
+	
--- core/Todo/views.py
+++ core/Todo/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import  Todo
 # Create your views here.
 
+tasks = {"task_1": "Убери комнату","task_2": "Купи продукты"}
+
 def main_page(request):
-	return HttpResponse("Hello World")
+	todos = Todo.objects.all()
+	print(todos)
+	return render(request,"todo/index.html", {'todos': todos})
 	
 def about_page(request):
 	return HttpResponse("Привет Привет")
